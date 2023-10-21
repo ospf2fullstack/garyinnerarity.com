eefc5998b6dd31e9f7246ff2285f7db4a61abcb2
ospf2fullstack <garyinnerarity@gmail.com>
2023-10-20 23:33:00 -0500

    dashboard work

---
+++ dash.js
@@ -0,0 +1,36 @@
+// date & time data
+document.addEventListener("DOMContentLoaded", function() {
+    function updateDateTime() {
+        const now = new Date();
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        document.getElementById('currentDate').textContent = now.toLocaleDateString('en-US', dateOptions);
+        document.getElementById('currentTime').textContent = now.toLocaleTimeString('en-US');
+    }
+
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+});
+
+// button function to load iframes, or whatever content
+document.querySelectorAll('.showGraphButton').forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Hide all iframes
+        document.querySelectorAll('.graph iframe').forEach(function(iframe) {
+            iframe.style.display = 'none';
+        });
+
+        // Show the associated iframe
+        var iframeId = button.getAttribute('data-iframe');
+        document.getElementById(iframeId).style.display = 'block';
+    });
+});
+
+// Title of page selected refreshes page
+document.getElementById('pageTitle').addEventListener('click', function() {
+    // Hide all iframes
+    document.querySelectorAll('.graph iframe').forEach(function(iframe) {
+        iframe.style.display = 'none';
+    });
+
+    // Reset any other changes to the view as needed
+});
