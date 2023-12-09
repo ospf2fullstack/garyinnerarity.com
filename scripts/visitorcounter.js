96fa059848b2638b58381e1a0d706dd219caf50c
Gary Innerarity <garyinnerarity@gmail.com>
2023-12-09 16:29:52 -0600

    counter

--- scripts/visitorcounter.js
+++ scripts/visitorcounter.js
@@ -1,7 +1,11 @@
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://xqhgsd62ea.execute-api.us-east-1.amazonaws.com/Production/visitorCount');
 xhr.onload = function() {
-    console.log('Lambda function triggered successfully');
+    if (xhr.status >= 200 && xhr.status < 300) {
+        const response = xhr.responseText;
+        document.getElementById('visitorcount').textContent = response;
+    }
+    // console.log('Lambda function triggered successfully');
 };
 xhr.onerror = function(err) {
     console.error('Error triggering Lambda function, you may be trying to bypass my CORS policy :)... or this:', err);
